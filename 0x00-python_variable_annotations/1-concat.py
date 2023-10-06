3dfc81449a49025abd3d4310176a209c1d7e3872
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-10-06 07:19:09 -0700

    Annotation-concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+""" Basic concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Returns sum of two strings 
+
+        Concatenate two strings and return the result.
+
+        Args:
+        str1 (str): The first string to be concatenated.
+        str2 (str): The second string to be concatenated.
+
+        Returns:
+            str: The concatenated string.
+    """
+    return str1 + str2
